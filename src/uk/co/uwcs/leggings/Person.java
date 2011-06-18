f6225441758fef093dc9570fffe635850e105bba
samu <Sam@.(none)>
2011-06-19 00:15:37 +0100

    some climbing stuff

--- src/uk/co/uwcs/leggings/Person.java
+++ src/uk/co/uwcs/leggings/Person.java
@@ -18,7 +18,9 @@ public class Person extends Lego{
 
 	private int walkcycle =0;
 	private String type;
-
+	
+	Boolean climbing = false;
+	
 	public Person(PApplet p, float x, float y) {
 		this(p, x, y, "default");
 	}
@@ -26,8 +28,8 @@ public class Person extends Lego{
 	public Person(PApplet p, float x, float y, String type) {
 		facing = 1;
 		parent = p;
-		this.x = x*HEIGHT;
-		this.y = y*WIDTH;
+		this.x = x*WIDTH;
+		this.y = y*HEIGHT;
 		this.width = 1;
 		this.height = 4;
 		this.type = type;
@@ -35,7 +37,7 @@ public class Person extends Lego{
 	public int update(Brick[][] collisionMap) {
 		
 		/******************************animation stuff*************************************/
-		if (type.equals("walking")){
+		if (type.equals("walking")||type.equals("climber")){
 			animation.update(1/parent.frameRate);
 			if (animation.isOver()){
 				walkcycle++;
@@ -70,26 +72,39 @@ public class Person extends Lego{
 				down = true;
 			}
 		}
-
+		
+		int front;
+		if(facing == 1) front = (int) (x/WIDTH + width);
+		else front = (int) (x/WIDTH + width)-1;
+		
 		for(int i=0; i<=height; ++i) {
-			int front;
-			if(facing == 1) front = (int) (x/WIDTH + width);
-			else front = (int) (x/WIDTH + width)-1;
 			if(collisionMap[front][(int) (y/HEIGHT)+i] != null) {
 				forwards[i] = true;
-				System.out.println(i+"is coliding");
+//				System.out.println(i+"is coliding");
 				if(i==(height-1))step = true;
 				else if(i==height)ledge = true;
 				else ahead = true;
 			}
 		}
 		if(type.equals("climber")) {
-			System.out.println("ahead: " + ahead + "; step: " + step + "; ledge: " + ledge);
-			if(ahead || step || ledge) {
+//			System.out.println("ahead: " + ahead + "; step: " + step + "; ledge: " + ledge);
+			if(ahead){
+				System.out.println("1");
 				y -= 30/parent.frameRate;
-			} else if(!down) {
+				climbing=true;
+			}else if (step && climbing){
+				y -= 30/parent.frameRate;		
+			} else if (ledge && climbing ){
+				System.out.println("2");
+				y -= 30/parent.frameRate;
+			}else if(!down) {
+				System.out.println("3");
 				y += 40/parent.frameRate;
-			} else {
+			}else if (step){
+				y-=HEIGHT;
+			}else {
+				climbing = false;
+				System.out.println("4");
 				x += facing*30/parent.frameRate;
 			}
 		} else {
@@ -113,6 +128,10 @@ public class Person extends Lego{
 		parent.pushMatrix(); 
 		parent.scale(facing,1);
 		if (type.equals("walking")){
+			sprite =images.get("sprite").get(walkcycle*54, 0, 54, 128);
+			parent.image(sprite,facing*x,y+2,(int)(facing*width*WIDTH*1.5),height*HEIGHT);
+
+		}else if (type.equals("climber")){
 			sprite =images.get("sprite").get(walkcycle*54, 0, 54, 128);
 			parent.image(sprite,facing*x,y+2,(int)(facing*width*WIDTH*1.5),height*HEIGHT);
 
