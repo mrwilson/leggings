9f50f27554fad5dc2c2f0bae6bc4a5f1da85dd82
Ben Falconer <ben@falconers.me.uk>
2011-06-18 02:20:59 +0100

    removed accidental import

--- src/uk/co/uwcs/leggings/Person.java
+++ src/uk/co/uwcs/leggings/Person.java
@@ -4,26 +4,39 @@ import java.util.HashMap;
 
 import processing.core.PApplet;
 import processing.core.PImage;
-import sun.applet.Main;
 
 public class Person extends Lego{
 	private PApplet parent;
-	private float x,y,height,width;
+	private float x,y;
+	private int height,width;
+	private boolean facing;
 	static HashMap<String,PImage> images = new HashMap<String, PImage>();
 
 	public Person(PApplet p, float x, float y) {
+		facing = true;
+		parent = p;
 		this.x = x;
 		this.y = y;
+		this.width = 1;
 		this.height = 4;
-		this.width = 2;
-		parent = p;
 	}
 	public void update(Brick[][] collisionMap) {
 		y += 10/parent.frameRate;
-		
-		/*if(x*HEIGHT+height <= ) {
-			
-		}*/
+		/*
+		boolean[] forwards = new boolean[height];
+		boolean[] downwards = new boolean[width];
+
+		for(int j=0; j<width; ++j) {
+			int bottom = (int) (y*HEIGHT + height);
+			if(collisionMap[(int) (x*WIDTH)+j][bottom] != null) {
+				downwards
+			}
+		}
+
+		for(int i=0; i<height; ++i) {
+		}
+
+		*/
 	}
 
 	public void draw() {
